d547b508d3a8dcd9cb688d944d233f32540c494f
Andres Aravena <andres.aravena@gmail.com>
2020-06-30 10:51:12 +0300

    long_loop, one_loop: when short timing is not enough

---
+++ long_loop.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+BIN=~/gmail-tools
+LBL=$1; shift
+if test -z "$LBL"; then
+  echo "Usage: $0 LABEL [options]"
+  exit 1
+fi
+
+echo Label is $LBL
+
+while true; do
+	date
+	/usr/local/bin/python3 $BIN/attach_downloader.py --log log.txt --label "$LBL" $* | tee new.txt
+	if test -s new.txt
+	then
+	  # awk -vLBL="$LBL" -f $BIN/messg.awk new.txt | sh
+	  cat new.txt >> log.txt
+	  echo NOW sleeping
+	  sleep 3600
+	else
+	  echo NOW sleeping
+	  sleep 7200
+        fi
+done
---
+++ one_loop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+BIN=~/gmail-tools
+LBL=$1; shift
+if test -z "$LBL"; then
+  echo "Usage: $0 LABEL [options]"
+  exit 1
+fi
+
+echo Label is $LBL
+
+date
+/usr/local/bin/python3 $BIN/attach_downloader.py --log log.txt --label "$LBL" $* | tee new.txt
+if test -s new.txt
+then
+  # awk -vLBL="$LBL" -f $BIN/messg.awk new.txt | sh
+  cat new.txt >> log.txt
+  echo Done
+fi
+date
